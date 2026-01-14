902e28cf93399c1a820a8374d2e951093cd82644
gojochan <ocean.arnab.02@gmail.com>
2026-01-14 12:52:27 +0530

    Initial voice assistant implementation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from logging import exception
+
 import speech_recognition as sr
 import pyttsx3
 
@@ -6,9 +8,23 @@ engine = pyttsx3.init()
 def say(text):
     engine.say(text)
     engine.runAndWait()
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold=1
+        audio = r.listen(source)
+        try:
+            query=r.recognize_google(audio,language='en-in')
+            print(f"user said: {query}")
+            return query
+        except exception as e:
+            return "some error occurred . sorry from lilo"
 
 if __name__ == "__main__":
     print("PyCharm")
-    say("Hello, I am Lilo AI")
+    say("Hello, I love mommy")
+    print("listening...")
+    query=takecommand()
+    say(query)
 
 
